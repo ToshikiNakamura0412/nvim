88543c5638ec0ff6ed5ee9a5a4cf921482787fdb
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-01-14 15:18:28 +0900

    Update install_prerequisites.sh

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Prerequisites
+if [ ! -e ~/.local/share/fonts/HackNerdFont-Regular.ttf ]; then
+    curl -fovLO ~/.local/share/fonts/HackNerdFont-Regular.ttf --create-dirs \
+        https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/Hack/Regular/HackNerdFont-Regular.ttf
+fi
 sudo apt-get update && sudo apt-get install -y --no-install-recommends \
     curl \
     wget \
@@ -16,7 +20,7 @@ sudo npm install -y -g n
 sudo n stable
 
 # Neovim
-sudo wget -O /opt/nvim.appimage https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+sudo wget -vO /opt/nvim.appimage https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 sudo chmod +x /opt/nvim.appimage
 sudo ln -sf /opt/nvim.appimage /usr/bin/vim
 sudo ln -sf /opt/nvim.appimage /usr/bin/nvim
