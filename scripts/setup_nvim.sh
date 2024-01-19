68614153b538059188585ef71b6e5daba3001044
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-01-19 19:18:08 +0900

    Add submodule vim (#5)

--- scripts/setup_nvim.sh
+++ scripts/setup_nvim.sh
@@ -7,11 +7,11 @@ if [ ! -d ~/.config/nvim ]; then
     mkdir -pv ~/.config/nvim
 fi
 ln -sfv $SCRIPT_DIR/../init.vim ~/.config/nvim/init.vim
-ln -sfv $SCRIPT_DIR/../configs/basic-settings.vim ~/.config/nvim/basic-settings.vim
-ln -sfv $SCRIPT_DIR/../configs/keymap.vim ~/.config/nvim/keymap.vim
-ln -sfv $SCRIPT_DIR/../configs/plugin-keymap.vim ~/.config/nvim/plugin-keymap.vim
-ln -sfv $SCRIPT_DIR/../configs/plugin-settings.vim ~/.config/nvim/plugin-settings.vim
-ln -sfv $SCRIPT_DIR/../configs/plugin.vim ~/.config/nvim/plugin.vim
+ln -sfv $SCRIPT_DIR/../configs/basic/basic-settings.vim ~/.config/nvim/basic-settings.vim
+ln -sfv $SCRIPT_DIR/../configs/basic/keymap.vim ~/.config/nvim/keymap.vim
+ln -sfv $SCRIPT_DIR/../configs/plugin/plugin-keymap.vim ~/.config/nvim/plugin-keymap.vim
+ln -sfv $SCRIPT_DIR/../configs/plugin/plugin-settings.vim ~/.config/nvim/plugin-settings.vim
+ln -sfv $SCRIPT_DIR/../configs/plugin/plugin.vim ~/.config/nvim/plugin.vim
 ln -sfv $SCRIPT_DIR/../coc-settings.json ~/.config/nvim/coc-settings.json
 ln -sfv $SCRIPT_DIR/../clang-format ~/.clang-format
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
