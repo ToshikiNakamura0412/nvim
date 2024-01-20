86bfb970259daee24d27bb53e5e9e8ccb237f01d
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-01-20 22:06:50 +0900

    Move files

--- scripts/setup_nvim.sh
+++ scripts/setup_nvim.sh
@@ -12,8 +12,8 @@ ln -sfv $SCRIPT_DIR/../configs/basic/keymap.vim ~/.config/nvim/keymap.vim
 ln -sfv $SCRIPT_DIR/../configs/plugin/plugin-keymap.vim ~/.config/nvim/plugin-keymap.vim
 ln -sfv $SCRIPT_DIR/../configs/plugin/plugin-settings.vim ~/.config/nvim/plugin-settings.vim
 ln -sfv $SCRIPT_DIR/../configs/plugin/plugin.vim ~/.config/nvim/plugin.vim
-ln -sfv $SCRIPT_DIR/../coc-settings.json ~/.config/nvim/coc-settings.json
-ln -sfv $SCRIPT_DIR/../clang-format ~/.clang-format
+ln -sfv $SCRIPT_DIR/../configs/others/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -sfv $SCRIPT_DIR/../configs/others/clang-format ~/.clang-format
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c 'PlugInstall --sync' -c qa
