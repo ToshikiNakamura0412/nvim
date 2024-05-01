b08e89ed3d3d347390898166e2bda0fcc07b9d40
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-05-01 09:18:20 +0900

    Update setup_nvim.sh (#15)

--- scripts/setup_nvim.sh
+++ scripts/setup_nvim.sh
@@ -14,8 +14,8 @@ ln -sfv $SCRIPT_DIR/../configs/plugin/plugin-settings.vim ~/.config/nvim/plugin-
 ln -sfv $SCRIPT_DIR/../configs/plugin/plugin.vim ~/.config/nvim/plugin.vim
 ln -sfv $SCRIPT_DIR/../configs/others/coc-settings.json ~/.config/nvim/coc-settings.json
 ln -sfv $SCRIPT_DIR/../configs/others/clang-format ~/.clang-format
-# curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # vim -c 'PlugInstall --sync' -c qa
 echo ">>> Done"
 echo ""
