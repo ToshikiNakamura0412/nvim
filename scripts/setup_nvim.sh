2ca2bd5d593389ce8da5c4bdc6ee73d45a0c3885
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-08-29 16:24:06 +0900

    Fix

--- scripts/setup_nvim.sh
+++ scripts/setup_nvim.sh
@@ -4,13 +4,13 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 echo ""
 echo "setting Neovim... "
 if [ -d ~/.local/share/nvim/plugged ]; then
-  ¦ rm -rf ~/.local/share/nvim/plugged
+    rm -rf ~/.local/share/nvim/plugged
 fi
 if [ -d ~/.config/nvim/undo ]; then
-  ¦ rm -rf ~/.config/nvim/undo
+    rm -rf ~/.config/nvim/undo
 fi
 if [ -d ~/.config/coc ]; then
-  ¦ rm -rf ~/.config/coc
+    rm -rf ~/.config/coc
 fi
 if [ ! -d ~/.config/nvim ]; then
     mkdir -pv ~/.config/nvim
