5dcc400be030f076bbcf36e215858419de69b305
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-08-29 19:30:37 +0900

    Create install_nvim_macos_arm64.sh

---
+++ scripts/install_nvim_macos_arm64.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-macos-arm64.tar.gz
+tar xzf nvim-macos-arm64.tar.gz
+if [ -d /opt/nvim-macos-arm64 ]; then
+  sudo rm -rf /opt/nvim-macos-arm64
+fi
+sudo mv nvim-macos-arm64 /opt/
+rm nvim-macos-arm64.tar.gz
+sudo ln -sfv /opt/nvim-macos-arm64/bin/nvim /usr/local/bin/nvim
