3a6e251ed67fddbea827b5633681c44ba294b0e3
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-08-29 20:18:09 +0900

    Fix

--- install_macos_arm64.sh
+++ install_macos_arm64.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 echo ""
 echo "install prerequisites..."
-$SCRIPT_DIR/scripts/install_prerequisites.sh
+$SCRIPT_DIR/scripts/install_prerequisites_macos_arm64.sh
 echo ">>> Done"
 echo ""
 $SCRIPT_DIR/scripts/setup_nvim.sh
