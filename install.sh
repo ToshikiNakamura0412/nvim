1dfccca5b6a9549a346e7ef529879cdcc6864ef1
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-08-30 17:40:52 +0900

    Support OS diff (#29)
    * Support OS diff
    
    * Remove files
    
    * Update README.md

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
+OS_NAME=$1
+if [ -z $OS_NAME ]; then
+    source /etc/os-release
+    OS_NAME=$(echo $ID)
+fi
 
 echo ""
 echo "install prerequisites..."
-$SCRIPT_DIR/scripts/install_prerequisites.sh
+$SCRIPT_DIR/scripts/install_prerequisites.sh $OS_NAME
 echo ">>> Done"
 echo ""
+
 $SCRIPT_DIR/scripts/setup_nvim.sh
-echo ""
-echo "==="
-echo "Finish!!"
-echo "==="
