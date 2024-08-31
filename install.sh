a48a966d3a669eb53777ac93a900fb91173b7c5b
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-08-31 22:45:35 +0900

    Add acquisition OS name for mac

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-OS_NAME=$1
-if [ -z $OS_NAME ]; then
+if [ "$(uname)" = "Linux" ]; then
     source /etc/os-release
     OS_NAME=$(echo $ID)
+elif [ "$(uname)" = "Darwin" ]; then
+    OS_NAME="mac"
+else
+    echo "This OS is not supported."
+    exit 1
 fi
 
 echo ""
 echo "install prerequisites..."
-$SCRIPT_DIR/scripts/install_prerequisites.sh $OS_NAME
+$SCRIPT_DIR/scripts/install_prerequisites.sh
 echo ">>> Done"
 echo ""
 
