0e129f14b0d81e3407a89a9171eb0de8744a6c4b
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-09-06 04:07:42 +0900

    Fix format (#36)
    * Fix format
    
    * Rename variable

--- scripts/install_prerequisites.sh
+++ scripts/install_prerequisites.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-if [ "$(uname)" = "Linux" ]; then
+MINI_PACK_BASE=(
+    curl
+    xsel
+    python3
+    neovim
+)
+MINI_PACK_PIP=(
+    pynvim
+    jedi
+)
+
+if [[ "$(uname)" == "Linux" ]]; then
     source /etc/os-release
-    OS_NAME=$(echo $ID)
-elif [ "$(uname)" = "Darwin" ]; then
-    OS_NAME="mac"
+    DISTRO=$(echo $ID)
+elif [[ "$(uname)" == "Darwin" ]]; then
+    DISTRO="mac"
 else
     echo "This OS is not supported."
     exit 1
 fi
 
-if [ $OS_NAME = "ubuntu" ] || [ $OS_NAME = "debian" ]; then
+if [[ ${DISTRO} == "ubuntu" ]] || [[ ${DISTRO} == "debian" ]]; then
     # Prerequisites
     sudo apt-get update && sudo apt-get install -y --no-install-recommends \
         curl \
@@ -31,18 +41,18 @@ if [ $OS_NAME = "ubuntu" ] || [ $OS_NAME = "debian" ]; then
         https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/Hack/Regular/HackNerdFont-Regular.ttf
 
     # Neovim
-    if [ $(arch) = "x86_64" ]; then
+    if [[ $(arch) == "x86_64" ]]; then
         sudo wget -vO /opt/nvim.appimage https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
         sudo chmod +x /opt/nvim.appimage
         sudo ln -sf /opt/nvim.appimage /usr/bin/nvim
-    elif [ $(arch) = "aarch64" ]; then
+    elif [[ $(arch) == "aarch64" ]]; then
         # Choose between appimage or build
         APP_TYPE="appimage"
-        if [ $APP_TYPE = "appimage" ]; then
+        if [[ ${APP_TYPE} == "appimage" ]]; then
             sudo wget -vO /opt/nvim.appimage https://github.com/matsuu/neovim-aarch64-appimage/releases/download/v0.10.1/nvim-v0.10.1-aarch64.appimage
             sudo chmod +x /opt/nvim.appimage
             sudo ln -sf /opt/nvim.appimage /usr/local/bin/nvim
-        elif [ $APP_TYPE = "build" ]; then
+        elif [[ ${APP_TYPE} == "build" ]]; then
             sudo apt-get update && sudo apt-get install -y --no-install-recommends \
                 unzip \
                 gettext \
@@ -58,37 +68,22 @@ if [ $OS_NAME = "ubuntu" ] || [ $OS_NAME = "debian" ]; then
         fi
     fi
 
-elif [ $OS_NAME = "alpine" ]; then
-    sudo apk update && sudo apk add --no-cache \
-        curl \
-        xsel \
-        python3 \
-        py3-pip \
-        neovim
-    pip3 install pynvim jedi
+elif [[ ${DISTRO} == "alpine" ]]; then
+    sudo apk update && sudo apk add --no-cache ${MINI_PACK_BASE[@]} py3-pip
+    pip3 install ${MINI_PACK_PIP[@]}
 
-elif [ $OS_NAME = "fedora" ]; then
-    sudo dnf check-update || true && sudo dnf install -y --setopt=install_weak_deps=False \
-        curl \
-        xsel \
-        python3 \
-        python3-pip \
-        neovim
-    pip3 install pynvim jedi
+elif [[ ${DISTRO} == "fedora" ]]; then
+    sudo dnf check-update || true && sudo dnf install -y --setopt=install_weak_deps=False ${MINI_PACK_BASE[@]} python3-pip
+    pip3 install ${MINI_PACK_PIP[@]}
 
-elif [ $OS_NAME = "opensuse-leap" ]; then
-    sudo zypper refresh && sudo zypper install -y --no-recommends \
-        curl \
-        xsel \
-        python3 \
-        python3-pip \
-        neovim
-    pip3 install pynvim jedi
+elif [[ ${DISTRO} == "opensuse-leap" ]]; then
+    sudo zypper refresh && sudo zypper install -y --no-recommends ${MINI_PACK_BASE[@]} python3-pip
+    pip3 install ${MINI_PACK_PIP[@]}
 
-elif [ $OS_NAME = "mac" ]; then
+elif [[ ${DISTRO} == "mac" ]]; then
     # Prerequisites
-    pip3 install pynvim jedi
-    if [ ! -d /Library/Fonts ]; then
+    pip3 install ${MINI_PACK_PIP[@]}
+    if [[ ! -d /Library/Fonts ]]; then
         sudo mkdir -pv /Library/Fonts
     fi
     cd /Library/Fonts && sudo curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/Hack/Regular/HackNerdFont-Regular.ttf
@@ -96,7 +91,7 @@ elif [ $OS_NAME = "mac" ]; then
     # Neovim
     curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-macos-arm64.tar.gz
     tar xzf nvim-macos-arm64.tar.gz
-    if [ -d /opt/nvim-macos-arm64 ]; then
+    if [[ -d /opt/nvim-macos-arm64 ]]; then
         sudo rm -rf /opt/nvim-macos-arm64
     fi
     sudo mv nvim-macos-arm64 /opt/
