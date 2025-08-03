b7091830d8b8372acd49c2a1876ea2afc8d8bd57
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2025-08-03 22:35:13 +0900

    Modify format (#41)
    * Modify format
    
    * Add pre-installation plugin manager
    
    * Modify comment

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
+NVIM_INSTALL_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-echo ""
-echo "install prerequisites..."
-$SCRIPT_DIR/scripts/install_prerequisites.sh
-echo ">>> Done"
-echo ""
+nvim_install() {
+  local script_dir="${NVIM_INSTALL_SCRIPT_DIR}"
+  "${script_dir}/scripts/install_prerequisites.sh"
+  "${script_dir}/scripts/setup_nvim.sh"
+}
 
-$SCRIPT_DIR/scripts/setup_nvim.sh
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+  nvim_install
+fi
--- scripts/setup_nvim.sh
+++ scripts/setup_nvim.sh
@@ -1,31 +1,38 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
+NVIM_SETUP_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-echo ""
-echo "setting Neovim... "
-if [[ -d ~/.local/share/nvim/plugged ]]; then
-    rm -rf ~/.local/share/nvim/plugged
-fi
-if [[ -d ~/.config/nvim/undo ]]; then
-    rm -rf ~/.config/nvim/undo
-fi
-if [[ -d ~/.config/coc ]]; then
-    rm -rf ~/.config/coc
-fi
-if [[ ! -d ~/.config/nvim ]]; then
-    mkdir -pv ~/.config/nvim
-fi
-ln -sfv ${SCRIPT_DIR}/../init.vim ~/.config/nvim/init.vim
-ln -sfv ${SCRIPT_DIR}/../configs/basic/basic-settings.vim ~/.config/nvim/basic-settings.vim
-ln -sfv ${SCRIPT_DIR}/../configs/basic/keymap.vim ~/.config/nvim/keymap.vim
-ln -sfv ${SCRIPT_DIR}/../configs/plugin/plugin-keymap.vim ~/.config/nvim/plugin-keymap.vim
-ln -sfv ${SCRIPT_DIR}/../configs/plugin/plugin-settings.vim ~/.config/nvim/plugin-settings.vim
-ln -sfv ${SCRIPT_DIR}/../configs/plugin/plugin.vim ~/.config/nvim/plugin.vim
-ln -sfv ${SCRIPT_DIR}/../configs/others/coc-settings.json ~/.config/nvim/coc-settings.json
-ln -sfv ${SCRIPT_DIR}/../configs/others/clang-format ~/.clang-format
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+nvim_setup() {
+  local script_dir="${NVIM_SETUP_SCRIPT_DIR}"
+
+  echo ""
+  echo "[INFO] setting Neovim... "
+  if [[ -d ~/.local/share/nvim/plugged ]]; then
+      rm -rf ~/.local/share/nvim/plugged
+  fi
+  if [[ -d ~/.config/nvim/undo ]]; then
+      rm -rf ~/.config/nvim/undo
+  fi
+  if [[ -d ~/.config/coc ]]; then
+      rm -rf ~/.config/coc
+  fi
+  if [[ ! -d ~/.config/nvim ]]; then
+      mkdir -pv ~/.config/nvim
+  fi
+  ln -sfv ${script_dir}/../init.vim ~/.config/nvim/init.vim
+  ln -sfv ${script_dir}/../configs/basic/basic-settings.vim ~/.config/nvim/basic-settings.vim
+  ln -sfv ${script_dir}/../configs/basic/keymap.vim ~/.config/nvim/keymap.vim
+  ln -sfv ${script_dir}/../configs/plugin/plugin-keymap.vim ~/.config/nvim/plugin-keymap.vim
+  ln -sfv ${script_dir}/../configs/plugin/plugin-settings.vim ~/.config/nvim/plugin-settings.vim
+  ln -sfv ${script_dir}/../configs/plugin/plugin.vim ~/.config/nvim/plugin.vim
+  ln -sfv ${script_dir}/../configs/others/coc-settings.json ~/.config/nvim/coc-settings.json
+  ln -sfv ${script_dir}/../configs/others/clang-format ~/.clang-format
+  curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# vim -c 'PlugInstall --sync' -c qa
-echo ">>> Done"
-echo ""
+  echo "[INFO] >>> Done"
+  echo ""
+}
+
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+  nvim_setup
+fi
