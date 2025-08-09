38dd204b65c28280664a9bca3b7950d153879632
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2025-08-09 22:35:02 +0900

    Add command existence check

--- scripts/extract_no_fuse_nvim_app.sh
+++ scripts/extract_no_fuse_nvim_app.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# If nvim command is not found, exit with an error message
+if ! command -v nvim >/dev/null 2>&1; then
+    echo "[ERROR] Neovim is not installed. Please install Neovim first."
+    exit 1
+fi
+
 mkdir -pv ~/.local/opt ~/.local/bin
 cd ~/.local/opt && nvim --appimage-extract
 ln -sfv ~/.local/opt/squashfs-root/usr/bin/nvim ~/.local/bin/nvim
+echo "[INFO] Installed Neovim for non-FUSE environments."
